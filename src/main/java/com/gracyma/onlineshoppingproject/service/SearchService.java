b9a4d099ec068ea26adbb4fceabba3507100d2bd
ClaraLee1 <97991874+ClaraLee1@users.noreply.github.com>
2024-11-28 13:03:43 -0800

    Implement SearchServiceTest to validate Rekognition API logic

--- src/main/java/com/gracyma/onlineshoppingproject/service/SearchService.java
+++ src/main/java/com/gracyma/onlineshoppingproject/service/SearchService.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.rekognition.model.DetectLabelsResult;
 import com.amazonaws.services.rekognition.model.Image;
 import com.amazonaws.services.rekognition.model.Label;
 import com.gracyma.onlineshoppingproject.common.AmzRekoClientProvider;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.io.IOException;
@@ -13,7 +14,9 @@ import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.ArrayList;
 
+@Service
 public class SearchService {
 
     @Resource
@@ -22,31 +25,21 @@ public class SearchService {
     public List<String> imageToLabels(String imagePath, int maxLabels){
 
         AmazonRekognition rekognitionClient = amzRekoClientProvider.getRekognitionClient();;
-
-
-        //String imagePath = "/home/taiwei/mp/Group-12-project/iphone.jpg";
         try {
             ByteBuffer imageBytes = ByteBuffer.wrap(Files.readAllBytes(Paths.get(imagePath)));
             DetectLabelsRequest request = new DetectLabelsRequest()
                     .withImage(new Image().withBytes(imageBytes))
                     .withMaxLabels(10)
                     .withMinConfidence(75F);
-
-            //DetectLabelsResult result = rekognitionClient.detectLabels(request);
             DetectLabelsResult result = rekognitionClient.detectLabels(request);
 
-
-            StringBuilder response = new StringBuilder("Detected Labels:\n");
+            List<String> labels = new ArrayList<>();
             for (Label label : result.getLabels()) {
-//                response.append(label.getName())
-//                        .append(": ")
-//                        .append(label.getConfidence())
-//                        .append("%\n");
-                System.out.println(label.toString());
+                labels.add(label.getName());
             }
+            return labels;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to process image: " + e.getMessage(), e);
         }
-        return null;
     }
 }
